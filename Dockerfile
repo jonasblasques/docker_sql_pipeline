72355ef629823b796628ec6b2a12dd5c937179b9
Jonas Blasques <tilakapash@gmail.com>
2025-05-02 16:01:41 +0100

    Setup pipeline to run on day specified in docker run argv[1]

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN pip install pandas
 
 WORKDIR /app
 COPY pipeline.py pipeline.py
-ENTRYPOINT [ "bash" ]
+ENTRYPOINT [ "python", "pipeline.py" ]
--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,15 @@
+import sys
+
 import pandas as pd
+
+sys.argv
+
+print (sys.argv)
+
+day = sys.argv[1]
+
 
 #some fancy stuff with pandas
 
-print("This is the pipeline.py file. Job finished successfully.")
+print(f'This is the pipeline.py file. Job finished successfully for day = f{day}')
 
